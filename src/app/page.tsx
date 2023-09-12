61f9f5fb2fff61a818b67d550d62e5d4e5d10a2f
Brett Gill <hello@brettgill.io>
2023-09-12 16:27:54 -0700

    change nav to icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,27 +13,30 @@ export default function Home() {
       <p className="text-xs">Post Feed</p>
       <div>
         {posts.map((post) => (
-          <div key={post.id}>
-            {post.user.avatar === 'image' ? (
-              <div className="flex justify-center items-center">
-                <img
-                  src={post.user?.avatar || "/placeholder.png"}
-                  alt="Post Image"
-                  className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
-                />
-              </div>
-            ) : (
-              <div className="flex justify-center items-center">
-                <img
-                  src={post.user?.avatar || "/placeholder.png"}
-                  alt="User Image"
-                  className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
-                />
-              </div>
-            )}
-            <h2 className='mt-4'>{post.user?.username || "Anonymous User"}</h2>
-            <p>{post.content}</p>
-          </div>
+  <div className="post-container w-[75%] mx-auto my-4 p-4 rounded-lg shadow-md">
+  <div key={post.id}>
+    {post.user.avatar === 'image' ? (
+      <div className="flex justify-center items-center">
+        <img
+          src={post.user?.avatar || "/placeholder.png"}
+          alt="Post Image"
+          className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
+        />
+      </div>
+    ) : (
+      <div className="flex justify-center items-center">
+        <img
+          src={post.user?.avatar || "/placeholder.png"}
+          alt="User Image"
+          className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
+        />
+      </div>
+    )}
+    <h2 className='mt-4'>{post.user?.username || "Anonymous User"}</h2>
+    <p>{post.content}</p>
+  </div>
+</div>
+
         ))}
       </div>
     </div>

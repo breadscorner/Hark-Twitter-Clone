30f58f1e67ca976b2c4b50e3458dae2821550793
Brett Gill <hello@brettgill.io>
2023-09-12 21:42:31 -0700

    add images to posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,37 +9,44 @@ export default function Home() {
 
   return (
     <div className="text-center">
-      <h1 className="text-lg">Home</h1>
-      <p className="text-xs">Post Feed</p>
+
       <div>
         {posts.map((post) => (
-  <div className="post-container w-[75%] mx-auto my-4 p-4 rounded-lg shadow-md">
-  <div key={post.id}>
-    <div className='flex items-center'>
+          <div className="post-container w-[75%] mx-auto my-4 p-4 rounded-lg shadow-md">
+            <div key={post.id}>
+              <div className='flex items-center'>
 
-    {post.user.avatar === 'image' ? (
-      <div className="flex justify-center items-center">
-        <img
-          src={post.user?.avatar || "/placeholder.png"}
-          alt="Post Image"
-          className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
-          />
-      </div>
-    ) : (
-      <div className="flex justify-center items-center">
-        <img
-          src={post.user?.avatar || "/placeholder.png"}
-          alt="User Image"
-          className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
-          />
-      </div>
-    )}
-    <h2 className='mt-4 ml-4'>{post.user?.username || "Anonymous User"}</h2>
-    </div>
-    <p className='mt-4 text-left'>{post.content}</p>
-  </div>
-</div>
-
+                {post.user.avatar === 'image' ? (
+                  <div className="flex justify-center items-center">
+                    <img
+                      src={post.user?.avatar || "/placeholder.png"}
+                      alt="Post Image"
+                      className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
+                    />
+                  </div>
+                ) : (
+                  <div className="flex justify-center items-center">
+                    <img
+                      src={post.user?.avatar || "/placeholder.png"}
+                      alt="User Image"
+                      className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
+                    />
+                  </div>
+                )}
+                <h2 className='mt-4 ml-4'>{post.user?.username || "Anonymous User"}</h2>
+              </div>
+              <p className='mt-4 text-left'>{post.content}</p>
+            </div>
+            {post.media?.url ? (
+              <div className="flex justify-center items-center">
+                <img
+                  src={post.media?.url}
+                  alt="Post Image"
+                  className="rounded-lg object-cover"
+                />
+              </div>
+            ) : null}
+          </div>
         ))}
       </div>
     </div>

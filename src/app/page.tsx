ca8ef8b3bce141d628cb61de5933f11b07788089
Brett Gill <hello@brettgill.io>
2023-09-12 21:27:34 -0700

    fix post avatar and user name css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,15 @@ export default function Home() {
         {posts.map((post) => (
   <div className="post-container w-[75%] mx-auto my-4 p-4 rounded-lg shadow-md">
   <div key={post.id}>
+    <div className='flex items-center'>
+
     {post.user.avatar === 'image' ? (
       <div className="flex justify-center items-center">
         <img
           src={post.user?.avatar || "/placeholder.png"}
           alt="Post Image"
           className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
-        />
+          />
       </div>
     ) : (
       <div className="flex justify-center items-center">
@@ -29,11 +31,12 @@ export default function Home() {
           src={post.user?.avatar || "/placeholder.png"}
           alt="User Image"
           className="w-[75px] h-[75px] rounded-full border-slate-500 border-[1px] mt-4"
-        />
+          />
       </div>
     )}
-    <h2 className='mt-4'>{post.user?.username || "Anonymous User"}</h2>
-    <p>{post.content}</p>
+    <h2 className='mt-4 ml-4'>{post.user?.username || "Anonymous User"}</h2>
+    </div>
+    <p className='mt-4 text-left'>{post.content}</p>
   </div>
 </div>
 

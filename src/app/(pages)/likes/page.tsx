933433b4d3b53460c32eabe9683099734d30372c
Brett Gill <hello@brettgill.io>
2023-11-21 20:51:49 -0800

    add an icon for unliking a post to likes page

--- src/app/(pages)/likes/page.tsx
+++ src/app/(pages)/likes/page.tsx
@@ -3,6 +3,7 @@ import PostIcons from "@/app/components/molecules/post-icons"
 import Image from "next/image"
 import { auth } from "@/utils/auth"
 import { redirect } from "next/navigation"
+import Unlike from "@/app/components/atoms/unlike-icon"
 
 export default async function ProfilePage() {
   const session = await auth();
@@ -19,22 +20,29 @@ export default async function ProfilePage() {
         <div className="font-[1.5em]">Liked Posts</div>
         {posts?.map((post) => (
           <div className="post-container w-[90%] md:w-full mx-auto my-4 px-4 border rounded-lg shadow-lg" key={post.id}>
-            <div className='flex items-center'>
-              <div className="relative w-[100px] h-[100px] rounded-full object-cover overflow-hidden mt-4 border-slate-500 border-[1px]"
-                style={{ width: '125px', height: '125px' }}>
-                {/* Can't use ProfileImage Component due to client side page */}
-                <Image
-                  src={post.user.profileImage || "../placeholder.png"}
-                  alt="Post Image"
-                  layout="fill"
-                  className="object-cover" />
-              </div>
-              {/* Post Header */}
-              <div className='flex flex-col text-left ml-8'>
-                <h2 className='mt-4 font-semibold text-[2em]'>{post.title}</h2>
-                <p className='mt-2 italic text-[1em]'>{post.user.username}</p>
-              </div>
-            </div>
+            <div className="flex items-center">
+  <div className="relative w-[100px] h-[100px] rounded-full object-cover overflow-hidden mt-4 border-slate-500 border-[1px]" style={{ width: '125px', height: '125px' }}>
+    {/* Can't use ProfileImage Component due to client side page */}
+    <Image
+      src={post.user.profileImage || "../placeholder.png"}
+      alt="Post Image"
+      layout="fill"
+      className="object-cover"
+    />
+  </div>
+
+  {/* Post Header */}
+  <div className="flex flex-col text-left ml-8">
+    <h2 className="mt-4 font-semibold text-[2em]">{post.title}</h2>
+    <p className="mt-2 italic text-[1em]">{post.user.username}</p>
+  </div>
+
+  {/* Container for Unlike Component */}
+  <div className="ml-auto pr-8">
+    <Unlike />
+  </div>
+</div>
+
 
             {/* Post Content */}
             <div className='border-t md:ml-[165px] md:mr-8'>
